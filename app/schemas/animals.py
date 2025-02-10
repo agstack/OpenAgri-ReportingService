650241944e809314d1011fd138d1c6489e2227bd
JoleVLF <Jovan@DESKTOP-JTIACNO>
2025-02-10 09:33:56 +0100

    Animal Reporting

---
+++ app/schemas/animals.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel, Field
+
+
+class AnimalGroup(BaseModel):
+    hasName: str
+
+
+class HasAgriParcel(BaseModel):
+    id: str = Field(alias="@id")
+
+
+class Animal(BaseModel):
+    id: str = Field(alias="@id")
+    nationalID: str
+    name: str
+    description: str
+    hasAgriParcel: Optional[HasAgriParcel] = None
+    sex: int
+    castrated: bool
+    species: str
+    breed: str
+    birthdate: datetime
+    isMemberOfAnimalGroup: Optional[AnimalGroup]
+    status: int
+    invalidatedAtTime: Optional[datetime]
+    dateCreated: datetime
+    dateModified: datetime
