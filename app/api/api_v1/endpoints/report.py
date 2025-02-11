de0d097a75eb8674e9d3eb99efbbeabce5fc02b3
JoleVLF <Jovan@DESKTOP-JTIACNO>
2025-02-11 11:33:13 +0100

    clean report

--- app/api/api_v1/endpoints/report.py
+++ app/api/api_v1/endpoints/report.py
@@ -4,7 +4,5 @@ from sqlalchemy.orm import Session
 import crud
 from api import deps
 from models import User
-from schemas import ReportCreate, Message, ReportDBID
-from utils.json_handler import ReportHandler
 
 router = APIRouter()
