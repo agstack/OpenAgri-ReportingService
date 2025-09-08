1a761531c4e7a4ea91bddf3d66119e88b9e1161e
JoleVLF <Jovan@DESKTOP-JTIACNO>
2025-09-08 16:59:22 +0200

    refactor: Format utils

--- app/utils/animals_report.py
+++ app/utils/animals_report.py
@@ -29,7 +29,10 @@ def parse_animal_data(data: Union[List[dict], str]) -> Optional[List[Animal]]:
         return None
 
 
-def create_pdf_from_animals(animals: List[Animal], token: dict[str, str],):
+def create_pdf_from_animals(
+    animals: List[Animal],
+    token: dict[str, str],
+):
     """
     Create PDF report from animal records
     """
@@ -43,7 +46,13 @@ def create_pdf_from_animals(animals: List[Animal], token: dict[str, str],):
     pdf.set_font("FreeSerif", "B", 14)
     pdf.cell(0, 10, f"Animal Data Report", ln=True, align="C")
     pdf.set_font("FreeSerif", style="", size=9)
-    pdf.cell(0, 7, f"Data Generated - {datetime.now().strftime('%d/%m/%Y')}", ln=True, align="C")
+    pdf.cell(
+        0,
+        7,
+        f"Data Generated - {datetime.now().strftime('%d/%m/%Y')}",
+        ln=True,
+        align="C",
+    )
     pdf.ln(5)
 
     pdf.set_font("FreeSerif", "B", 12)
@@ -57,17 +66,21 @@ def create_pdf_from_animals(animals: List[Animal], token: dict[str, str],):
     if len(animals) == 1:
         an = animals[0]
         parcel_id = an.hasAgriParcel.id if an.hasAgriParcel else None
-        address = ''
-        farm  = ''
+        address = ""
+        farm = ""
         if parcel_id:
             parcel = parcel_id.split(":")[3]
             if parcel:
-                address, farm = get_parcel_info(parcel_id.split(":")[-1], token, geolocator)
+                address, farm = get_parcel_info(
+                    parcel_id.split(":")[-1], token, geolocator
+                )
 
         pdf.set_font("FreeSerif", "B", 10)
         pdf.cell(40, 8, "Created:")
         pdf.set_font("FreeSerif", "", 10)
-        pdf.multi_cell(0, 8, f"{an.dateCreated.strftime('%d/%m/%Y')}", ln=True, fill=True)
+        pdf.multi_cell(
+            0, 8, f"{an.dateCreated.strftime('%d/%m/%Y')}", ln=True, fill=True
+        )
 
         pdf.set_font("FreeSerif", "B", 10)
         pdf.cell(40, 8, "Parcel Location:")
@@ -75,22 +88,44 @@ def create_pdf_from_animals(animals: List[Animal], token: dict[str, str],):
         pdf.multi_cell(0, 8, address, ln=True, fill=True)
 
         pdf.set_font("FreeSerif", "B", 10)
-        pdf.cell(40, 8, "Farm information:", )
+        pdf.cell(
+            40,
+            8,
+            "Farm information:",
+        )
         pdf.set_font("FreeSerif", "", 10)
         pdf.multi_cell(0, 8, farm, ln=True, fill=True)
 
         pdf.set_font("FreeSerif", "B", 10)
-        pdf.cell(40, 8, "Animal:", )
+        pdf.cell(
+            40,
+            8,
+            "Animal:",
+        )
         pdf.set_font("FreeSerif", "", 10)
-        pdf.multi_cell(0, 8, f"Name: {an.name}, Sex: {an.sex}, Birthdate {an.birthdate.strftime('%d/%m/%Y')}", ln=True, fill=True)
+        pdf.multi_cell(
+            0,
+            8,
+            f"Name: {an.name}, Sex: {an.sex}, Birthdate {an.birthdate.strftime('%d/%m/%Y')}",
+            ln=True,
+            fill=True,
+        )
 
         pdf.set_font("FreeSerif", "B", 10)
-        pdf.cell(40, 8, "Species:", )
+        pdf.cell(
+            40,
+            8,
+            "Species:",
+        )
         pdf.set_font("FreeSerif", "", 10)
         pdf.multi_cell(0, 8, an.species, ln=True, fill=True)
 
         pdf.set_font("FreeSerif", "B", 10)
-        pdf.cell(40, 8, "Castrated:", )
+        pdf.cell(
+            40,
+            8,
+            "Castrated:",
+        )
         pdf.set_font("FreeSerif", "", 10)
         pdf.multi_cell(0, 8, an.isCastrated, ln=True, fill=True)
 
@@ -113,27 +148,30 @@ def create_pdf_from_animals(animals: List[Animal], token: dict[str, str],):
             pdf.set_font("FreeSerif", "", 9)
             for animal in animals:
                 row = table.row()
-                row.cell(animal.dateCreated.strftime('%d/%m/%Y'))
+                row.cell(animal.dateCreated.strftime("%d/%m/%Y"))
                 row.cell(animal.name)
                 row.cell(animal.description)
 
-                address = ''
-                parcel_id = (
-                        animal.hasAgriParcel.id if animal.hasAgriParcel else None
-                )
+                address = ""
+                parcel_id = animal.hasAgriParcel.id if animal.hasAgriParcel else None
                 if parcel_id:
                     parcel = parcel_id.split(":")[3]
                     if parcel:
-                        address, _ = get_parcel_info(parcel_id.split(":")[-1], token, geolocator)
+                        address, _ = get_parcel_info(
+                            parcel_id.split(":")[-1], token, geolocator
+                        )
 
                 row.cell(address)
                 row.cell(animal.species)
                 row.cell(
                     f"{'Male' if animal.sex == 0 else 'Female'} | Castrated: {animal.isCastrated}",
                 )
-                row.cell(animal.birthdate.strftime('%d/%m/%Y'))
-                row.cell(f"{animal.invalidatedAtTime if animal.invalidatedAtTime else 'N/A'}")
-                row.cell(f"{animal.isMemberOfAnimalGroup.hasName if animal.isMemberOfAnimalGroup else 'N/A'}"
+                row.cell(animal.birthdate.strftime("%d/%m/%Y"))
+                row.cell(
+                    f"{animal.invalidatedAtTime if animal.invalidatedAtTime else 'N/A'}"
+                )
+                row.cell(
+                    f"{animal.isMemberOfAnimalGroup.hasName if animal.isMemberOfAnimalGroup else 'N/A'}"
                 )
                 pdf.ln(10)
 
@@ -141,10 +179,13 @@ def create_pdf_from_animals(animals: List[Animal], token: dict[str, str],):
 
 
 def process_animal_data(
-        token: dict[str, str], pdf_file_name: str, params: dict | None = None, data=None,
-        from_date: datetime.date = None,
-        to_date: datetime.date = None,
-        farm_animal_id: str = None
+    token: dict[str, str],
+    pdf_file_name: str,
+    params: dict | None = None,
+    data=None,
+    from_date: datetime.date = None,
+    to_date: datetime.date = None,
+    farm_animal_id: str = None,
 ) -> None:
     """
     Process animal data and generate PDF report
@@ -160,7 +201,7 @@ def process_animal_data(
 
     else:
         if params:
-            params["format"]= "json"
+            params["format"] = "json"
             decode_dates_filters(params, from_date, to_date)
             json_data = make_get_request(
                 url=f'{settings.REPORTING_FARMCALENDAR_BASE_URL}{settings.REPORTING_FARMCALENDAR_URLS["animals"]}',
@@ -175,9 +216,8 @@ def process_animal_data(
                 json_data = make_get_request(
                     url=f'{settings.REPORTING_FARMCALENDAR_BASE_URL}{settings.REPORTING_FARMCALENDAR_URLS["animals"]}',
                     token=token,
-                    params={'format': "json"},
+                    params={"format": "json"},
                 )
-
 
     animals = parse_animal_data(json_data)
     try:
--- app/utils/irrigation_report.py
+++ app/utils/irrigation_report.py
@@ -29,7 +29,9 @@ def parse_irrigation_operations(data: dict) -> Optional[List[IrrigationOperation
         )
 
 
-def create_pdf_from_operations(operations: List[IrrigationOperation], token: dict[str, str] = None):
+def create_pdf_from_operations(
+    operations: List[IrrigationOperation], token: dict[str, str] = None
+):
     """
     Create PDF report from irrigation operations
     """
@@ -43,7 +45,13 @@ def create_pdf_from_operations(operations: List[IrrigationOperation], token: dic
     pdf.set_font("FreeSerif", "B", 14)
     pdf.cell(0, 10, f"Irrigation Operation Report", ln=True, align="C")
     pdf.set_font("FreeSerif", style="", size=9)
-    pdf.cell(0, 7, f"Data Generated - {datetime.now().strftime('%d/%m/%Y')}", ln=True, align="C")
+    pdf.cell(
+        0,
+        7,
+        f"Data Generated - {datetime.now().strftime('%d/%m/%Y')}",
+        ln=True,
+        align="C",
+    )
     pdf.ln(5)
 
     pdf.set_font("FreeSerif", "B", 12)
@@ -56,18 +64,19 @@ def create_pdf_from_operations(operations: List[IrrigationOperation], token: dic
 
     if len(operations) == 1:
         op = operations[0]
-        parcel_id = (
-            op.operatedOn.get("@id") if op.operatedOn else None
-        )
-        address = ''
-        farm  = ''
+        parcel_id = op.operatedOn.get("@id") if op.operatedOn else None
+        address = ""
+        farm = ""
         if parcel_id:
             parcel = parcel_id.split(":")[3] if op.operatedOn else None
             if parcel:
-                address, farm = get_parcel_info(parcel_id.split(":")[-1], token, geolocator)
-        start_time = op.hasStartDatetime.strftime(
-            "%d/%m/%Y") if op.hasStartDatetime else ''
-        end_time = op.hasEndDatetime.strftime('%d/%m/%Y') if op.hasEndDatetime else ''
+                address, farm = get_parcel_info(
+                    parcel_id.split(":")[-1], token, geolocator
+                )
+        start_time = (
+            op.hasStartDatetime.strftime("%d/%m/%Y") if op.hasStartDatetime else ""
+        )
+        end_time = op.hasEndDatetime.strftime("%d/%m/%Y") if op.hasEndDatetime else ""
         pdf.set_font("FreeSerif", "B", 10)
         pdf.cell(40, 8, "Star-End :")
         pdf.set_font("FreeSerif", "", 10)
@@ -79,17 +88,35 @@ def create_pdf_from_operations(operations: List[IrrigationOperation], token: dic
         pdf.multi_cell(0, 8, address, ln=True, fill=True)
 
         pdf.set_font("FreeSerif", "B", 10)
-        pdf.cell(40, 8, "Farm information:", )
+        pdf.cell(
+            40,
+            8,
+            "Farm information:",
+        )
         pdf.set_font("FreeSerif", "", 10)
         pdf.multi_cell(0, 8, farm, ln=True, fill=True)
 
         pdf.set_font("FreeSerif", "B", 10)
-        pdf.cell(40, 8, "Value info:", )
+        pdf.cell(
+            40,
+            8,
+            "Value info:",
+        )
         pdf.set_font("FreeSerif", "", 10)
-        pdf.multi_cell(0, 8, f"{op.hasAppliedAmount.numericValue} ({op.hasAppliedAmount.unit})", ln=True, fill=True)
+        pdf.multi_cell(
+            0,
+            8,
+            f"{op.hasAppliedAmount.numericValue} ({op.hasAppliedAmount.unit})",
+            ln=True,
+            fill=True,
+        )
 
         pdf.set_font("FreeSerif", "B", 10)
-        pdf.cell(40, 8, "Responsible agent:", )
+        pdf.cell(
+            40,
+            8,
+            "Responsible agent:",
+        )
         pdf.set_font("FreeSerif", "", 10)
         pdf.multi_cell(0, 8, op.responsibleAgent, ln=True, fill=True)
     if len(operations) > 1:
@@ -109,22 +136,28 @@ def create_pdf_from_operations(operations: List[IrrigationOperation], token: dic
             for op in operations:
                 # Operation Header
                 row = table.row()
-                start_time = op.hasStartDatetime.strftime(
-                    "%d/%m/%Y") if op.hasStartDatetime else ''
-                end_time = op.hasEndDatetime.strftime('%d/%m/%Y') if op.hasEndDatetime else ''
+                start_time = (
+                    op.hasStartDatetime.strftime("%d/%m/%Y")
+                    if op.hasStartDatetime
+                    else ""
+                )
+                end_time = (
+                    op.hasEndDatetime.strftime("%d/%m/%Y") if op.hasEndDatetime else ""
+                )
                 row.cell(f"{start_time} - {end_time}")
                 row.cell(op.details)
 
-                parcel_id = (
-                    op.operatedOn.get("@id") if op.operatedOn else None
-                )
-                address = ''
+                parcel_id = op.operatedOn.get("@id") if op.operatedOn else None
+                address = ""
                 if parcel_id:
                     parcel = parcel_id.split(":")[3] if op.operatedOn else None
                     if parcel:
-                        address, _ = get_parcel_info(parcel_id.split(":")[-1], token, geolocator)
+                        address, _ = get_parcel_info(
+                            parcel_id.split(":")[-1], token, geolocator
+                        )
                 row.cell(address)
-                row.cell(f"{op.hasAppliedAmount.numericValue} ({op.hasAppliedAmount.unit})",
+                row.cell(
+                    f"{op.hasAppliedAmount.numericValue} ({op.hasAppliedAmount.unit})",
                 )
 
                 row.cell(op.usesIrrigationSystem)
@@ -136,8 +169,14 @@ def create_pdf_from_operations(operations: List[IrrigationOperation], token: dic
     return pdf
 
 
-def process_irrigation_data(data, token: dict[str, str], pdf_file_name: str, from_date: datetime.date = None,
-                            to_date: datetime.date = None,irrigation_id: str = None) -> None:
+def process_irrigation_data(
+    data,
+    token: dict[str, str],
+    pdf_file_name: str,
+    from_date: datetime.date = None,
+    to_date: datetime.date = None,
+    irrigation_id: str = None,
+) -> None:
     """
     Process irrigation data and generate PDF report
     """
@@ -146,7 +185,7 @@ def process_irrigation_data(data, token: dict[str, str], pdf_file_name: str, fro
         json_data = make_get_request(
             url=f'{settings.REPORTING_FARMCALENDAR_BASE_URL}{settings.REPORTING_FARMCALENDAR_URLS["irrigations"]}{irrigation_id}/',
             token=token,
-            params={"format": "json"}
+            params={"format": "json"},
         )
 
         json_data = [json_data] if json_data else None
